09ecee2ea77c13e4cfaacc8fc01d615afd91a0ce
Виктор <Irzunovv@gmail.com>
2024-11-05 16:24:45 +0300

    -sharp

--- next.config.js
+++ next.config.js
@@ -5,26 +5,18 @@ module.exports = {
 		 {
 			protocol: 'http',
 			hostname: 'localhost',
-			port: '3000', // Include port if needed
-			pathname: '/uploads/**', // Allow all images in the /uploads/ directory
+			port: '3000', 
+			pathname: '/uploads/**',
+		 },
+		 {
+			protocol: 'https',
+			hostname: process.env.NEXT_PUBLIC_BASE_URL || 'автокар.бел',
+			pathname: '/uploads/**',
 		 },
 	  ],
 	},
 	env: {
-	  NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || 'http://автокар.бел',
+	  NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || 'https://автокар.бел',
 	},
  };
  
-
-//  images: {
-// 	remotePatterns: [
-// 	  {
-// 		 protocol: 'https',
-// 		 hostname: process.env.NEXT_PUBLIC_HOSTNAME || 'domen.by', // use production domain
-// 		 pathname: '/uploads/**', // Allow all images in the /uploads/ directory
-// 	  },
-// 	],
-//  },
-//  env: {
-// 	NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL || 'https://domen.by',
-//  },

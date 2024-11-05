2b8a3a434640e39a4a63ecba3575fcf1e440bc24
Виктор <Irzunovv@gmail.com>
2024-11-05 22:32:06 +0300

    -images err next.config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	images: {
-	  remotePatterns: [
-		 {
-			protocol: 'https',
-			hostname: 'автокар.бел',
-			pathname: '/uploads/**',
-		 },
-	  ],
+	  domains: ['автокар.бел'],
 	},
  };
  

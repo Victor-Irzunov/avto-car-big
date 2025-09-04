adda6ceb1abf073db42599908451d4e1119509e8
Виктор <Irzunovv@gmail.com>
2025-09-04 20:56:25 +0300

    phone edit-3

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-	  domains: ['localhost'],
-	},
 	// images: {
-	//   domains: ['xn--80aaf6atok.xn--90ais'],
+	//   domains: ['localhost'],
 	// },
+	images: {
+	  domains: ['xn--80aaf6atok.xn--90ais'],
+	},
  };
  
  module.exports = nextConfig;

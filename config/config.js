45750b66ef08e6df15264f971c450f52fd868404
Виктор <Irzunovv@gmail.com>
2025-09-04 20:37:32 +0300

    phone edit

--- config/config.js
+++ config/config.js
@@ -1,10 +1,10 @@
 const phoneNumbers = {
-	mainPhone: '+375 29 663-17-83',
-	mainPhoneLink: '+375296631783',
-	secondaryPhone: '+375 29 256-86-29',
-	secondaryPhoneLink: '+375292568629',
-	thirdPhone: '+375 33 355-88-55',
-	thirdPhoneLink: '+375333558855',
+	mainPhone: '+375 29 112-62-22',
+	mainPhoneLink: '+375291126222',
+	secondaryPhone: '+375 33 355-88-55',
+	secondaryPhoneLink: '+375333558855',
+	thirdPhone: '',
+	thirdPhoneLink: '',
  };
  
  export default phoneNumbers;

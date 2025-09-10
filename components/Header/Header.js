b680a35a6d28f18b521ecb5d0fe659d4da9a208f
Виктор <Irzunovv@gmail.com>
2025-09-10 07:53:28 +0300

    err page-11

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -10,13 +10,8 @@ export default function Header() {
   const pathname = usePathname()
   const [drawerOpen, setDrawerOpen] = useState(false);
 
-  const toggleDrawer = () => {
-    setDrawerOpen(!drawerOpen);
-  };
-
-  const closeDrawer = () => {
-    setDrawerOpen(false);
-  };
+  const toggleDrawer = () => setDrawerOpen(!drawerOpen);
+  const closeDrawer = () => setDrawerOpen(false);
 
   return (
     <header className="z-40 w-full xz:fixed sd:relative top-2">
@@ -27,28 +22,20 @@ export default function Header() {
           <svg className="w-10 h-10 animate-ringing" fill="currentColor" viewBox="0 0 24 24">
             <path d="M6.62 10.79a15.91 15.91 0 006.59 6.59l2.2-2.2a1 1 0 011.1-.23 11.36 11.36 0 003.55.57 1 1 0 011 1v3.55a1 1 0 01-1 1A19.94 19.94 0 012 4a1 1 0 011-1h3.55a1 1 0 011 1 11.36 11.36 0 00.57 3.55 1 1 0 01-.23 1.1z"></path>
           </svg>
-          <a href={`tel:${phoneNumbers.thirdPhoneLink}`} className=''>
-            {phoneNumbers.thirdPhone}
-          </a>
+          <a href={`tel:${phoneNumbers.thirdPhoneLink}`}>{phoneNumbers.thirdPhone}</a>
         </div>
       </div>
 
       <div className='days-one-regular sd:mt-4 xz:mt-5'>
         <div className="sd:container mx-auto">
           <div className="sd:py-3 xz:py-1 bg-white/90 rounded-xl sd:px-8 xz:px-3">
-
             <div className='flex justify-between items-center'>
               {/* Desktop Menu */}
               <nav className="xz:hidden sd:block w-full">
-                {
-                  pathname !== '/thank-you/' && pathname !== '/politika/' &&
+                {pathname !== '/thank-you/' && pathname !== '/politika/' && (
                   <ul className="flex items-center justify-between w-full text-xl p-0 tracking-wider font-semibold text-secondary">
-                    <li className=''>
-                      <Link
-                        href={`${process.env.NEXT_PUBLIC_BASE_URL}/`}
-                        prefetch={false}
-                        className="cursor-pointer hover:text-primary"
-                      >
+                    <li>
+                      <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/`} prefetch={false} className="cursor-pointer hover:text-primary">
                         <div className='text-secondary sd:block xz:hidden items-center text-3xl leading-none uppercase'>
                           <p className='mb-0 pb-0 h-4'>avto</p>
                           <span className='font-bold text-4xl pt-0 mt-0 h-0'>car</span>
@@ -56,60 +43,35 @@ export default function Header() {
                       </Link>
                     </li>
                     <li>
-                      <Link
-                        href={`${process.env.NEXT_PUBLIC_BASE_URL}/catalog/`}
-                        prefetch={false}
-                        className="cursor-pointer hover:text-primary"
-                      >
+                      <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/catalog/`} prefetch={false} className="cursor-pointer hover:text-primary">
                         Автомобили
                       </Link>
                     </li>
                     <li>
-                      <Link
-                        href={`${process.env.NEXT_PUBLIC_BASE_URL}/credit/`}
-                        prefetch={false}
-                        className="cursor-pointer hover:text-primary"
-                      >
+                      <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/credit/`} prefetch={false} className="cursor-pointer hover:text-primary">
                         Кредит
                       </Link>
                     </li>
                     <li>
-                      <Link
-                        href={`${process.env.NEXT_PUBLIC_BASE_URL}/lizing/`}
-                        prefetch={false}
-                        className="cursor-pointer hover:text-primary"
-                      >
+                      <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/lizing/`} prefetch={false} className="cursor-pointer hover:text-primary">
                         Лизинг
                       </Link>
                     </li>
                     <li>
-                      <Link
-                        href={`${process.env.NEXT_PUBLIC_BASE_URL}/obmen/`}
-                        prefetch={false}
-                        className="cursor-pointer hover:text-primary"
-                      >
+                      <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/obmen/`} prefetch={false} className="cursor-pointer hover:text-primary">
                         Обмен
                       </Link>
                     </li>
                     <li>
-                      <Link
-                        href={`${process.env.NEXT_PUBLIC_BASE_URL}/komissionnaya-prodazha/`}
-                        prefetch={false}
-                        className="cursor-pointer hover:text-primary"
-                        onClick={closeDrawer}
-                      >
+                      <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/komissionnaya-prodazha/`} prefetch={false} className="cursor-pointer hover:text-primary" onClick={closeDrawer}>
                         Комиссионная продажа
                       </Link>
                     </li>
                   </ul>
-                }
+                )}
               </nav>
 
-              <Link
-                href={`${process.env.NEXT_PUBLIC_BASE_URL}/`}
-                prefetch={false}
-                className=""
-              >
+              <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/`} prefetch={false} className="">
                 <div className="sd:hidden xz:inline-flex">
                   <div className='text-secondary items-center text-xl leading-none uppercase'>
                     <p className='mb-0 pb-0 h-3'>avto</p>
@@ -130,12 +92,8 @@ export default function Header() {
                   <button onClick={toggleDrawer} aria-label="Toggle menu">
                     <Image src='/svg/menu.svg' alt='Кнопка меню' width={35} height={35} />
                   </button>
-                  {/* Drawer Menu */}
-                  <div className={`
-                    fixed top-0 right-0 w-80 z-50 text-secondary bg-base-100 h-full transform
-                    ${drawerOpen ? 'translate-x-0' : 'translate-x-full'}
-                    transition-transform duration-300 ease-in-out
-                  `}>
+
+                  <div className={`fixed top-0 right-0 w-80 z-50 text-secondary bg-base-100 h-full transform ${drawerOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out`}>
                     <ul className="menu p-3 relative text-white text-xl font-semibold">
                       <li className="mb-4">
                         <div className='text-white flex flex-col items-center text-xl leading-none uppercase absolute right-3'>
@@ -144,72 +102,37 @@ export default function Header() {
                         </div>
                       </li>
                       <li className='pt-20 mb-2'>
-                        <Link
-                          href={`${process.env.NEXT_PUBLIC_BASE_URL}/catalog/`}
-                          prefetch={false}
-                          className="hover:text-primary"
-                          onClick={closeDrawer}
-                        >
+                        <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/catalog/`} prefetch={false} className="hover:text-primary" onClick={closeDrawer}>
                           Автомобили
                         </Link>
                       </li>
                       <li className='mb-2'>
-                        <Link
-                          href={`${process.env.NEXT_PUBLIC_BASE_URL}/vykup-avto/`}
-                          prefetch={false}
-                          className="hover:text-primary"
-                          onClick={closeDrawer}
-                        >
+                        <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/vykup-avto/`} prefetch={false} className="hover:text-primary" onClick={closeDrawer}>
                           Выкуп авто
                         </Link>
                       </li>
                       <li className='mb-2'>
-                        <Link
-                          href={`${process.env.NEXT_PUBLIC_BASE_URL}/komissionnaya-prodazha/`}
-                          prefetch={false}
-                          className="hover:text-primary"
-                          onClick={closeDrawer}
-                        >
+                        <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/komissionnaya-prodazha/`} prefetch={false} className="hover:text-primary" onClick={closeDrawer}>
                           Комиссионная продажа
                         </Link>
                       </li>
                       <li className='mb-2'>
-                        <Link
-                          href={`${process.env.NEXT_PUBLIC_BASE_URL}/credit/`}
-                          prefetch={false}
-                          className="hover:text-primary"
-                          onClick={closeDrawer}
-                        >
+                        <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/credit/`} prefetch={false} className="hover:text-primary" onClick={closeDrawer}>
                           Кредит
                         </Link>
                       </li>
                       <li className='mb-2'>
-                        <Link
-                          href={`${process.env.NEXT_PUBLIC_BASE_URL}/lizing/`}
-                          prefetch={false}
-                          className="hover:text-primary"
-                          onClick={closeDrawer}
-                        >
+                        <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/lizing/`} prefetch={false} className="hover:text-primary" onClick={closeDrawer}>
                           Лизинг
                         </Link>
                       </li>
                       <li className='mb-2'>
-                        <Link
-                          href={`${process.env.NEXT_PUBLIC_BASE_URL}/obmen/`}
-                          prefetch={false}
-                          className="hover:text-primary"
-                          onClick={closeDrawer}
-                        >
+                        <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/obmen/`} prefetch={false} className="hover:text-primary" onClick={closeDrawer}>
                           Обмен
                         </Link>
                       </li>
                       <li className='mb-2'>
-                        <Link
-                          href={`${process.env.NEXT_PUBLIC_BASE_URL}/prigon-iz-es/`}
-                          prefetch={false}
-                          className="hover:text-primary"
-                          onClick={closeDrawer}
-                        >
+                        <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/prigon-iz-es/`} prefetch={false} className="hover:text-primary" onClick={closeDrawer}>
                           Пригон из ЕС
                         </Link>
                       </li>
@@ -224,7 +147,7 @@ export default function Header() {
                       </div>
                       <div className='flex items-center space-x-2 mt-4 pl-1'>
                         <Image src='/svg/phone-white.svg' alt='Телефон автосалона' width={25} height={25} />
-                        <div className=''>
+                        <div>
                           <a href={`tel:${phoneNumbers.mainPhoneLink}`} className='font-semibold block'>
                             {phoneNumbers.mainPhone}
                           </a>
@@ -234,9 +157,8 @@ export default function Header() {
                         </div>
                       </div>
                     </div>
-                    <button onClick={closeDrawer} className="absolute top-4 left-4 text-4xl text-white">
-                      &times;
-                    </button>
+
+                    <button onClick={closeDrawer} className="absolute top-4 left-4 text-4xl text-white">&times;</button>
                   </div>
                 </div>
               </div>
--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 import Image from "next/image";
 import Link from "next/link";
 import phoneNumbers from "@/config/config";
@@ -12,124 +12,68 @@ const Footer = observer(() => {
   return (
     <footer className={`pt-10 pb-2 relative bg-[#2D3192] text-[#12142B]`} id='contacts'>
       <div className='container mx-auto'>
-
         <aside className='grid sd:grid-cols-4 xz:grid-cols-1 gap-4'>
-
           <div className='flex flex-col justify-between'>
-            <div className=''>
-              <Image
-                src='/logo/logo3.webp'
-                alt='Логотип автосалона - продажа авто в кредит и лизинг'
-                width={150} height={150}
-                className=""
-              />
+            <div>
+              <Image src='/logo/logo3.webp' alt='Логотип автосалона - продажа авто в кредит и лизинг' width={150} height={150} />
             </div>
           </div>
 
-          <div className=''>
+          <div>
             <p className='text-xl'>Разделы</p>
             <ul className="mt-1 grid grid-cols-1 xz:grid-cols-2 sd:grid-cols-1 gap-2">
-              <li className=''>
-                <Link
-                  href={`${process.env.NEXT_PUBLIC_BASE_URL}/`}
-                  prefetch={false}
-                  className="hover:text-primary"
-                >
+              <li>
+                <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/`} prefetch={false} className="hover:text-primary">
                   Автомобили
                 </Link>
               </li>
-              <li className=''>
-                <Link
-                  href={`${process.env.NEXT_PUBLIC_BASE_URL}/vykup-avto/`}
-                  prefetch={false}
-                  className="hover:text-primary"
-                >
+              <li>
+                <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/vykup-avto/`} prefetch={false} className="hover:text-primary">
                   Выкуп авто
                 </Link>
               </li>
-              <li className=''>
-                <Link
-                  href={`${process.env.NEXT_PUBLIC_BASE_URL}/komissionnaya-prodazha/`}
-                  prefetch={false}
-                  className="hover:text-primary"
-                >
+              <li>
+                <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/komissionnaya-prodazha/`} prefetch={false} className="hover:text-primary">
                   Комиссионная продажа
                 </Link>
               </li>
-              <li className=''>
-                <Link
-                  href={`${process.env.NEXT_PUBLIC_BASE_URL}/credit/`}
-                  prefetch={false}
-                  className="hover:text-primary"
-                >
+              <li>
+                <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/credit/`} prefetch={false} className="hover:text-primary">
                   Кредит
                 </Link>
               </li>
-              <li className=''>
-                <Link
-                  href={`${process.env.NEXT_PUBLIC_BASE_URL}/lizing/`}
-                  prefetch={false}
-                  className="hover:text-primary"
-                >
+              <li>
+                <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/lizing/`} prefetch={false} className="hover:text-primary">
                   Лизинг
                 </Link>
               </li>
-              <li className=''>
-                <Link
-                  href={`${process.env.NEXT_PUBLIC_BASE_URL}/obmen/`}
-                  prefetch={false}
-                  className="hover:text-primary"
-                >
+              <li>
+                <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/obmen/`} prefetch={false} className="hover:text-primary">
                   Обмен
                 </Link>
               </li>
-              <li className=''>
-                <Link
-                  href={`${process.env.NEXT_PUBLIC_BASE_URL}/prigon-iz-es/`}
-                  prefetch={false}
-                  className="hover:text-primary"
-                >
+              <li>
+                <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/prigon-iz-es/`} prefetch={false} className="hover:text-primary">
                   Пригон из ЕС
                 </Link>
               </li>
-              {/* <li className=''>
-                <Link
-                  href={`${process.env.NEXT_PUBLIC_BASE_URL}/traid-in/`}
-                  prefetch={false}
-                  className="hover:text-primary"
-                >
-                  Traid-in
-                </Link>
-              </li> */}
-              <li className=''>
-                {
-                  user.userData?.isAdmin ? (
-                    <Link
-                      href={`${process.env.NEXT_PUBLIC_BASE_URL}/super-admin/`}
-                      prefetch={false}
-                      className="cursor-pointer hover:text-primary"
-                    >
-                      <p className=''>Админ панель</p>
-                    </Link>
-                  ) : (
-                    <Link
-                      href={`${process.env.NEXT_PUBLIC_BASE_URL}/login/`}
-                      prefetch={false}
-                      className="cursor-pointer hover:text-primary"
-                    >
-                      <Image src='/svg/entrance.svg' alt='Вход' width={25} height={25} />
-                    </Link>
-                  )
-                }
+              <li>
+                {user.userData?.isAdmin ? (
+                  <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/super-admin/`} prefetch={false} className="cursor-pointer hover:text-primary">
+                    <p>Админ панель</p>
+                  </Link>
+                ) : (
+                  <Link href={`${process.env.NEXT_PUBLIC_BASE_URL}/login/`} prefetch={false} className="cursor-pointer hover:text-primary">
+                    <Image src='/svg/entrance.svg' alt='Вход' width={25} height={25} />
+                  </Link>
+                )}
               </li>
             </ul>
           </div>
 
           <div className='sd:mt-0 xz:mt-4'>
-            <div className=''>
-              <p className='text-xl'>Адрес</p>
-              <p className='mt-1'>Минск, ул. Куйбышева 40, Паркинг 4 этаж</p>
-            </div>
+            <p className='text-xl'>Адрес</p>
+            <p className='mt-1'>Минск, ул. Куйбышева 40, Паркинг 4 этаж</p>
             <div className='mt-4'>
               <p className='text-xl'>Почта</p>
               <p className='mt-1'>Avg30@bk.ru</p>
@@ -137,31 +81,23 @@ const Footer = observer(() => {
           </div>
 
           <div className='sd:mt-0 xz:mt-4'>
-            <div className=''>
-              <p className='text-xl'>Время</p>
-              <p className='mt-1'>С 10:00-20:00 Без выходных</p>
-            </div>
+            <p className='text-xl'>Время</p>
+            <p className='mt-1'>С 10:00-20:00 Без выходных</p>
             <div className='mt-4'>
               <p className='text-xl'>Телефон</p>
-              <a href={`tel:${phoneNumbers.mainPhoneLink}`} className='mt-1 block'>
-                {phoneNumbers.mainPhone} A1
-              </a>
-              <a href={`tel:${phoneNumbers.secondaryPhoneLink}`} className='mt-2 block'>
-                {phoneNumbers.secondaryPhone} МТС
-              </a>
+              <a href={`tel:${phoneNumbers.mainPhoneLink}`} className='mt-1 block'>{phoneNumbers.mainPhone} A1</a>
+              <a href={`tel:${phoneNumbers.secondaryPhoneLink}`} className='mt-2 block'>{phoneNumbers.secondaryPhone} МТС</a>
             </div>
           </div>
         </aside>
 
         <aside className='mt-8'>
-          <p className='text-xs'>
-            ООО «АВТОКАРГРУПП» УНП 193846922, г. Минск, ул. пер. С.Ковалевской, д.54 к.1 каб.303-127
-          </p>
+          <p className='text-xs'>ООО «АВТОКАРГРУПП» УНП 193846922, г. Минск, ул. пер. С.Ковалевской, д.54 к.1 каб.303-127</p>
           <div className='text-[10px] sd:mt-0 xz:mt-4'>
-            <p className=''>Банк: ЗАО «Альфа-Банк»,</p>
-            <p className=''>г. Минск, ул. Сурганова, 43-47</p>
-            <p className=''>БИК банка: ALFABY2X</p>
-            <p className=''>р/с: BY40 ALFA 3012 2G50 3400 1027 0000</p>
+            <p>Банк: ЗАО «Альфа-Банк»,</p>
+            <p>г. Минск, ул. Сурганова, 43-47</p>
+            <p>БИК банка: ALFABY2X</p>
+            <p>р/с: BY40 ALFA 3012 2G50 3400 1027 0000</p>
           </div>
         </aside>
       </div>
@@ -170,7 +106,7 @@ const Footer = observer(() => {
         <div className='container mx-auto text-center '>
           <p className='text-blue-950 text-[10px]'>
             Copyright © 2024 | Разработка и Продвижение
-            <a href='https://vi-tech.by' target='_blank' rel="noreferrer" className=''> VI:TECH</a>.
+            <a href='https://vi-tech.by' target='_blank' rel="noreferrer"> VI:TECH</a>.
             {' '}Информация на сайте не является публичной офертой и предоставляется исключительно в информационных целях.
           </p>
         </div>
